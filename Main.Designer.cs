ad805e0290a538ad929a9bbe9c16b9376cd01a68
Dale Fugier <dale@mcneel.com>
2013-03-06 10:30:53 -0800

    Initial commit

---
+++ Main.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace TestRhinoAutomation
+{
+  partial class Main
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.buttonRun = new System.Windows.Forms.Button();
+      this.buttonShow = new System.Windows.Forms.Button();
+      this.buttonOpen = new System.Windows.Forms.Button();
+      this.buttonHide = new System.Windows.Forms.Button();
+      this.buttonClose = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // buttonRun
+      // 
+      this.buttonRun.Location = new System.Drawing.Point(12, 12);
+      this.buttonRun.Name = "buttonRun";
+      this.buttonRun.Size = new System.Drawing.Size(201, 23);
+      this.buttonRun.TabIndex = 3;
+      this.buttonRun.Text = "Run Rhino";
+      this.buttonRun.UseVisualStyleBackColor = true;
+      this.buttonRun.Click += new System.EventHandler(this.buttonRun_Click);
+      // 
+      // buttonShow
+      // 
+      this.buttonShow.Location = new System.Drawing.Point(12, 42);
+      this.buttonShow.Name = "buttonShow";
+      this.buttonShow.Size = new System.Drawing.Size(201, 23);
+      this.buttonShow.TabIndex = 4;
+      this.buttonShow.Text = "Show Rhino";
+      this.buttonShow.UseVisualStyleBackColor = true;
+      this.buttonShow.Click += new System.EventHandler(this.buttonShow_Click);
+      // 
+      // buttonOpen
+      // 
+      this.buttonOpen.Location = new System.Drawing.Point(12, 72);
+      this.buttonOpen.Name = "buttonOpen";
+      this.buttonOpen.Size = new System.Drawing.Size(203, 23);
+      this.buttonOpen.TabIndex = 5;
+      this.buttonOpen.Text = "Open...";
+      this.buttonOpen.UseVisualStyleBackColor = true;
+      this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+      // 
+      // buttonHide
+      // 
+      this.buttonHide.Location = new System.Drawing.Point(13, 102);
+      this.buttonHide.Name = "buttonHide";
+      this.buttonHide.Size = new System.Drawing.Size(200, 23);
+      this.buttonHide.TabIndex = 6;
+      this.buttonHide.Text = "Hide Rhino";
+      this.buttonHide.UseVisualStyleBackColor = true;
+      this.buttonHide.Click += new System.EventHandler(this.buttonHide_Click);
+      // 
+      // buttonClose
+      // 
+      this.buttonClose.Location = new System.Drawing.Point(13, 132);
+      this.buttonClose.Name = "buttonClose";
+      this.buttonClose.Size = new System.Drawing.Size(200, 23);
+      this.buttonClose.TabIndex = 7;
+      this.buttonClose.Text = "Close Rhino";
+      this.buttonClose.UseVisualStyleBackColor = true;
+      this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+      // 
+      // Main
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(227, 173);
+      this.Controls.Add(this.buttonClose);
+      this.Controls.Add(this.buttonHide);
+      this.Controls.Add(this.buttonOpen);
+      this.Controls.Add(this.buttonShow);
+      this.Controls.Add(this.buttonRun);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
+      this.Name = "Main";
+      this.Text = "TestRhinoAutomation";
+      this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button buttonRun;
+    private System.Windows.Forms.Button buttonShow;
+    private System.Windows.Forms.Button buttonOpen;
+    private System.Windows.Forms.Button buttonHide;
+    private System.Windows.Forms.Button buttonClose;
+  }
+}
+
---
+++ Main.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Rhino4;
+using RhinoScript4;
+
+namespace TestRhinoAutomation
+{
+  public partial class Main : Form
+  {
+    IRhino5Application m_Rhino = null;
+    IRhinoScript m_RhinoScript = null;
+
+    public Main()
+    {
+      InitializeComponent();
+      InitializeButtons();
+    }
+
+    private void InitializeButtons()
+    {
+      bool bNoRhino = (null == m_Rhino);
+      buttonRun.Enabled = bNoRhino;
+      buttonShow.Enabled = !bNoRhino;
+      buttonOpen.Enabled = !bNoRhino;
+      buttonHide.Enabled = !bNoRhino;
+      buttonClose.Enabled = !bNoRhino;
+    }
+
+    private void ReleaseObjects()
+    {
+      if (null != m_RhinoScript)
+      {
+        m_RhinoScript.DocumentModified(0);
+        m_RhinoScript = null;
+      }
+
+      if (null != m_Rhino)
+      {
+        // Release all references to Rhino by setting its reference count to 0.
+        Marshal.FinalReleaseComObject(m_Rhino);
+        m_Rhino = null;
+      }
+    }
+
+    private void buttonRun_Click(object sender, EventArgs e)
+    {
+      try
+      {
+        Type type = Type.GetTypeFromProgID("Rhino5.Application");
+        object obj = Activator.CreateInstance(type);
+        m_Rhino = (IRhino5Application)obj;
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(string.Format("Failed to create Rhino5.Application.\n\nError = {0}", ex.Message));
+      }
+
+      InitializeButtons();
+    }
+
+    private void buttonShow_Click(object sender, EventArgs e)
+    {
+      try
+      {
+        m_Rhino.Visible = 1;
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(string.Format("Error = {0}", ex.Message));
+      }
+    }
+
+    private void buttonOpen_Click(object sender, EventArgs e)
+    {
+      if (null == m_RhinoScript)
+      {
+        try
+        {
+          object obj = m_Rhino.GetScriptObject();
+          m_RhinoScript = (IRhinoScript)obj;
+        }
+        catch (Exception ex)
+        {
+          MessageBox.Show(string.Format("Error = {0}", ex.Message));
+        }
+      }
+
+      if (null != m_RhinoScript)
+      {
+        m_RhinoScript.Command("_Open", 0);
+      }
+    }
+
+    private void buttonHide_Click(object sender, EventArgs e)
+    {
+      try
+      {
+        m_Rhino.Visible = 0;
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(string.Format("Error = {0}", ex.Message));
+      }
+    }
+
+    private void buttonClose_Click(object sender, EventArgs e)
+    {
+      ReleaseObjects();
+      InitializeButtons();
+    }
+
+    private void Main_FormClosing(object sender, FormClosingEventArgs e)
+    {
+      ReleaseObjects();
+    }
+  }
+}
